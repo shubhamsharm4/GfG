0342b50141419df4eeaa68d49f58f16e746b7a1a
Shubham <shubham.sharma4390@gmail.com>
2024-12-06 09:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find hIndex
+    int hIndex(vector<int>& arr) {
+        sort(arr.rbegin(), arr.rend());
+        int idx = 0;
+        while(idx < arr.size() && arr[idx] > idx) {
+            idx++;
+        }
+        
+        return idx;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        int result = ob.hIndex(arr);
+
+        cout << result << endl;
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
